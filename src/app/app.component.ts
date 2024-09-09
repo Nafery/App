41b0eabff54d797f0e117857c827613c66d91bfd
LaffyTC <th.gutierrez@duocuc.cl>
2024-09-09 18:57:59 -0300

    perfil
    solo diseño na' ta funcional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { addIcons } from 'ionicons';
+import { wallet, car, mail, settings, help, home, person } from 'ionicons/icons';
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,12 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    /**
+     * Any icons you want to use in your application
+     * can be registered in app.component.ts and then
+     * referenced by name anywhere in your application.
+     */
+    addIcons({ wallet, car, mail, settings, help, home, person });
+  }
 }
